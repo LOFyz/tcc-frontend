4b548c928e238105a3d22404c52fc51ac951d004
LOFyz <leonardo_farias2010@hotmail.com>
2021-07-01 13:51:02 -0300

    feat: eslint configuration done

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo } className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+
+
         </p>
         <a
+
           className="App-link"
           href="https://reactjs.org"
           target="_blank"

eb017768c9b61b080446b44933f0f34ba18a799f
LOFyz <leonardo_farias2010@hotmail.com>
2021-07-04 13:59:23 -0300

    Chore: navigation separated in a component

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+import React from 'react';
+
+const components: React.FC = () => {
+  return <Navigation />;
+};
+
+export default components;
+
+export const Navigation = styled.div`
+  background: #66bb6a;
+  width: 100%;
+  height: 7.2rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+
+  button {
+    background: none;
+    border: none;
+  }
+
+  svg {
+    width: 3rem;
+    height: 3rem;
+    color: #fff;
+  }
+
+  a {
+    display: none;
+  }
+
+  @media (orientation: landscape) {
+    width: 33.7rem;
+    height: 92.2rem;
+    background: rgba(102, 187, 106, 0.84);
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+
+    svg {
+      width: 4rem;
+      height: 4rem;
+    }
+
+    a {
+      display: initial;
+      font-family: Roboto;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 2.3rem;
+      text-decoration: none;
+      margin-left: 0.5rem;
+
+      color: #ffffff;
+    }
+    button {
+      width: 100%;
+      height: 7.368rem;
+      background: rgba(0, 0, 0, 0.06);
+      box-shadow: 0rem 0rem 0.4rem rgba(0, 0, 0, 0.03);
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: flex-start;
+      padding-left: 2.5rem;
+      margin-top: 0.4rem;
+    }
+  }
+`;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -65,6 +65,7 @@ export default Dashboard;
 
 import styled from 'styled-components';
 import NotificationButton from '../components/NotificationButton';
+import Navigation from '../components/Navigation';
 
 const Header = styled.div`
   width: 100%;
@@ -122,70 +123,7 @@ const Body = styled.div`
   }
 `;
 
-const Navigation = styled.div`
-  background: #66bb6a;
-  width: 100%;
-  height: 7.2rem;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-
-  button {
-    background: none;
-    border: none;
-  }
-
-  svg {
-    width: 3rem;
-    height: 3rem;
-    color: #fff;
-  }
-
-  a {
-    display: none;
-  }
-
-  @media (orientation: landscape) {
-    width: 33.7rem;
-    height: 92.2rem;
-    background: rgba(102, 187, 106, 0.84);
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: flex-start;
-
-    svg {
-      width: 4rem;
-      height: 4rem;
-    }
-
-    a {
-      display: initial;
-      font-family: Roboto;
-      font-style: normal;
-      font-weight: normal;
-      font-size: 2.3rem;
-      text-decoration: none;
-      margin-left: 0.5rem;
-
-      color: #ffffff;
-    }
-    button {
-      width: 100%;
-      height: 7.368rem;
-      background: rgba(0, 0, 0, 0.06);
-      box-shadow: 0rem 0rem 0.4rem rgba(0, 0, 0, 0.03);
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: flex-start;
-      padding-left: 2.5rem;
-      margin-top: 0.4rem;
-    }
-  }
-`;
-
-const Container = styled.div`
+export const Container = styled.div`
   display: flex;
   flex-direction: column;
 

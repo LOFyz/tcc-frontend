ef2100f853fbad348d73f3120e12c7a4e7bbfd99
LOFyz <leonardo_farias2010@hotmail.com>
2021-08-26 16:12:46 -0300

    FIx: BrowserRouter missing

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import { ThemeProvider } from '@material-ui/core';
 import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from './styles/theme';
+import { BrowserRouter } from 'react-router-dom';
 
 const App: React.FC = () => (
   <ThemeProvider theme={theme}>
     <StyledComponentsThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
       <GlobalStyle />
-      <Routes />
       <CssBaseline />
     </StyledComponentsThemeProvider>
   </ThemeProvider>

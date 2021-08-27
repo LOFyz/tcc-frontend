c62c12ac2339175c8053cf1df2482d8494b74452
LOFyz <leonardo_farias2010@hotmail.com>
2021-08-27 13:48:24 -0300

    Chore: modal in mobile mode

--- src/components/PostCreationModal.tsx
+++ src/components/PostCreationModal.tsx
@@ -100,7 +100,7 @@ const PostCreationModal: React.FC<modal> = ({ open, handleModal }) => {
             onSubmit={formik.handleSubmit}
             noValidate
           >
-            <Typography variant="h4" className={classes.title}>
+            <Typography variant="h4" className={classes.title} id="title">
               Adicione uma imagem.
             </Typography>
             <label>
@@ -183,6 +183,14 @@ const Container = styled.div`
   border: 1px solid rgba(0, 0, 0, 0.2);
   box-sizing: border-box;
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
+
+  @media (orientation: portrait) {
+    width: 80%;
+
+    #title {
+      font-size: 2rem;
+    }
+  }
 `;
 
 const AppBar = styled.div`

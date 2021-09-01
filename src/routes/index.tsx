a453b82c560bdf08a4acd58726b957bfc033fda1
LOFyz <leonardo_farias2010@hotmail.com>
2021-08-31 22:06:09 -0300

    Feat: Route of react router dom changed to route component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, BrowserRouter } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import SignUp from '../pages/SignUp';
@@ -8,17 +8,20 @@ import Profile from '../pages/Profile';
 import Schedule from '../pages/Schedule';
 import GroupCreation from '../pages/GroupCreation';
 import Group from '../pages/Group';
+import Route from './Route';
 
 const Routes: React.FC = () => (
-  <Switch>
-    <Route path="/" exact component={Dashboard} />
-    <Route path="/signup" component={SignUp} />
-    <Route path="/signin" component={SignIn} />
-    <Route path="/profile/:userId" component={Profile} />
-    <Route path="/schedule/:userId" component={Schedule} />
-    <Route path="/groupcreation" component={GroupCreation} />
-    <Route path="/group/:groupId" component={Group} />
-  </Switch>
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Dashboard} isPrivate />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/signin" component={SignIn} />
+      <Route path="/profile/:userId" component={Profile} isPrivate />
+      <Route path="/schedule/:userId" component={Schedule} isPrivate />
+      <Route path="/groupcreation" component={GroupCreation} isPrivate />
+      <Route path="/group/:groupId" component={Group} isPrivate />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default Routes;
